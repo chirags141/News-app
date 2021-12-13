4c8c295a838b47c0b87d2598a1c21192520f6082
Chirag Sethiya <chiragsethiya123@gmail.com>
2021-12-13 22:37:49 +0530

    Loading Spinner & Previous and Next button added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
       <div>
 
         <Navbar></Navbar>
-        <News></News>
+        <News pageSize={5}></News>
 
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -1,24 +1,90 @@
 import { Component } from "react";
 import React from "react";
 import { NewsItem } from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
+
+
+    constructor(){
+        super();
+        // console.log("Constructer of News")
+        this.state = {
+            articles: [],
+            loading : false,
+            page:1
+        }
+    }
+
+   async componentDidMount(){
+     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=1&pageSize=${this.props.pageSize}`
+     this.setState({loading : true })
+     let data = await fetch(url);
+     let parsedData =await data.json()
+    //  console.log(parsedData)
+     this.setState({
+       articles:parsedData.articles,
+       totalResults : parsedData.totalResults,
+       loading : false
+      })
+   }
+
+   handlePrevClick = async ()=>{
+    //  console.log("prev")
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true })
+    let data = await fetch(url);
+    let parsedData =await data.json()
+    // console.log(parsedData.articles)
+    this.setState({
+      page : this.state.page-1,
+      articles : parsedData.articles,
+      loading : false
+    })
+   }
+
+   handleNextClick = async ()=>{
+     if(!(this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
+
+      //  console.log("next");
+       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+       this.setState({loading : true })
+       let data = await fetch(url); 
+       let parsedData =await data.json();
+      //  console.log(parsedData.articles)
+       this.setState({
+         page : this.state.page+1,
+         articles : parsedData.articles,
+         loading : false
+       })
+     }
+   }
+
   render() {
     return (
       <div className="container my-3">
-        <h2>Sports Headlines</h2>
+        <h2 style={{textAlign:"center"}}> Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="Title1" description="myDescription" imageUrl=""></NewsItem>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="Title2" description="myDescription" imageUrl=""></NewsItem>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="Title3" description="myDescription" imageUrl=""></NewsItem>
-          </div>
+          {!this.state.loading && this.state.articles.map((element)=>{
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem 
+                title={element.title} description={element.description} 
+                imageUrl={element.urlToImage?element.urlToImage:"https://images.hindustantimes.com/img/2021/11/15/1600x900/Uttar_Pradesh_Purvanchal_Expressway_Narendra_Modi_1636988478195_1636988478374.jpg"}
+                newsUrl={element.url}>
+                </NewsItem>
+              </div>
+            )
+            })
+          }
+          
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&laquo; Previous</button>
+          <button disabled={this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
         </div>
       </div>
-    );
+    ); 
   }
 }
