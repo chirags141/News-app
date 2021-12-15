eb9ea1542e999edeb1dc543f45817f22aee618c5
Chirag Sethiya <chiragsethiya123@gmail.com>
2021-12-16 01:24:31 +0530

    Categories added and routing done

--- src/components/News.js
+++ src/components/News.js
@@ -2,9 +2,22 @@ import { Component } from "react";
 import React from "react";
 import { NewsItem } from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
 
+    static defaultProps = {
+      country: 'in',
+      pageSize : 5,
+      category : 'general'
+    }
+
+    static propTypes = {
+      country : PropTypes.string,
+      pageSize : PropTypes.number,
+      category : PropTypes.string,
+    }
 
     constructor(){
         super();
@@ -17,7 +30,7 @@ export class News extends Component {
     }
 
    async componentDidMount(){
-     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=1&pageSize=${this.props.pageSize}`
+     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=1&pageSize=${this.props.pageSize}`
      this.setState({loading : true })
      let data = await fetch(url);
      let parsedData =await data.json()
@@ -31,7 +44,7 @@ export class News extends Component {
 
    handlePrevClick = async ()=>{
     //  console.log("prev")
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
     this.setState({loading : true })
     let data = await fetch(url);
     let parsedData =await data.json()
@@ -47,7 +60,7 @@ export class News extends Component {
      if(!(this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
 
       //  console.log("next");
-       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f9deb4187114baeaf5ef6c23c88e6e4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
        this.setState({loading : true })
        let data = await fetch(url); 
        let parsedData =await data.json();
@@ -72,7 +85,10 @@ export class News extends Component {
                 <NewsItem 
                 title={element.title} description={element.description} 
                 imageUrl={element.urlToImage?element.urlToImage:"https://images.hindustantimes.com/img/2021/11/15/1600x900/Uttar_Pradesh_Purvanchal_Expressway_Narendra_Modi_1636988478195_1636988478374.jpg"}
-                newsUrl={element.url}>
+                newsUrl={element.url}
+                author = {element.author}
+                date= {element.publishedAt}
+                source = {element.source.name}>
                 </NewsItem>
               </div>
             )
